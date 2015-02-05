ee3ff963dbdc0a4e119a049cb4bf69fa03e6c71a
jnkroeker <jnkroeker@gmail.com>
2015-02-05 13:58:13 -0500

    LineItem save edit prepared to accept array

--- catexpense/CATEXPENSEFRONT/angular/app/scripts/controllers/submission.js
+++ catexpense/CATEXPENSEFRONT/angular/app/scripts/controllers/submission.js
@@ -345,6 +345,7 @@ angular.module('expenseApp')
           modalInstance.result.then(
               function (successMessage) {
                   if (LineItemService.getUnderEdit() == true) {
+                      successMessage.forEach(function (lineItem) {
                       LineItemService.updateLineItem(successMessage.LineItemId, successMessage).then(
                           function (success) {
                               if (Application.getAllUserSubmissions() != undefined) {
@@ -363,6 +364,7 @@ angular.module('expenseApp')
                             function (error) {
                                 console.log(error);
                             });
+                      });
                       LineItemService.setUnderEdit(false);
                   } else {
                       successMessage.forEach(function (lineItem) {
