4ca86a697ba4d267206df4eeabb678679a55d257
jnkroeker <jnkroeker@gmail.com>
2015-02-05 14:34:32 -0500

    save edit modification

--- catexpense/CATEXPENSEFRONT/angular/app/scripts/controllers/submission.js
+++ catexpense/CATEXPENSEFRONT/angular/app/scripts/controllers/submission.js
@@ -345,7 +345,6 @@ angular.module('expenseApp')
           modalInstance.result.then(
               function (successMessage) {
                   if (LineItemService.getUnderEdit() == true) {
-                      successMessage.forEach(function (lineItem) {
                       LineItemService.updateLineItem(successMessage.LineItemId, successMessage).then(
                           function (success) {
                               if (Application.getAllUserSubmissions() != undefined) {
@@ -364,7 +363,6 @@ angular.module('expenseApp')
                             function (error) {
                                 console.log(error);
                             });
-                      });
                       LineItemService.setUnderEdit(false);
                   } else {
                       successMessage.forEach(function (lineItem) {
