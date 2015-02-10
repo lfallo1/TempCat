2595cb4f017a8a994313b5b218e18608663235bd
jnkroeker <jnkroeker@gmail.com>
2015-02-10 13:23:22 -0500

    save api call errors to database

---
+++ catexpense/CATEXPENSEFRONT/Services/ErrorService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CatExpenseFront.Services.Interfaces;
+using CatExpenseFront.Repository;
+using CatExpenseFront.Models;
+
+namespace CatExpenseFront.Services
+{
+    public class ErrorService : IErrorService
+    {
+        private IRepository<Error> repository;
+
+        public ErrorService() { }
+
+        public ErrorService(IRepository<Error> irepository)
+        {
+            this.repository = irepository;
+        }
+
+        public Models.Error Create(Models.Error tobject)
+        {
+            return this.repository.Create(tobject);
+        }
+
+
+        public void SaveChanges()
+        {
+            this.repository.SaveChanges();
+        }
+
+        public IEnumerable<Models.Error> All()
+        {
+            return this.repository.All();
+        }
+
+        public int Update(Models.Error tobject)
+        {
+            return this.repository.Update(tobject);
+        }
+
+        public Models.Error Find(int id)
+        {
+            return this.repository.Find(id);
+        }
+
+        public int Delete(Models.Error tobject)
+        {
+            return this.repository.Delete(tobject);
+        }
+
+        public IEnumerable<Models.Error> CreateAll(IEnumerable<Models.Error> tobjects)
+        {
+            return this.repository.CreateAll(tobjects);
+        }
+    }
+}
---
+++ catexpense/CATEXPENSEFRONT/Services/Interfaces/IErrorService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CatExpenseFront.Models;
+
+namespace CatExpenseFront.Services.Interfaces
+{
+    public interface IErrorService: IService<Error>
+    {
+    }
+}
