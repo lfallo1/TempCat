42bed717829dec3fbe22d6e8cbd543641103ee8e
wboorman <warren.boorman@catalystitservices.com>
2015-02-20 16:25:09 -0500

    adding a submission page (only has header controls now) and submission modal (has nothing)

---
+++ catexpense/Selenium/PageObjects/SubmissionModal.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.PageObjects
+{
+    public class SubmissionModal : PageObjectBase
+    {
+        public SubmissionModal(IWebDriver driver) : base (driver)
+        {
+        }
+    }
+}
