507ce4d9c7a9aa25688531ff3b576fd965a0f760
jnkroeker <jnkroeker@gmail.com>
2015-02-20 10:34:49 -0500

    fix expense policy bug

--- catexpense/CATEXPENSEFRONT/angular/app/scripts/controllers/modals/policyView.js
+++ catexpense/CATEXPENSEFRONT/angular/app/scripts/controllers/modals/policyView.js
@@ -14,20 +14,42 @@ angular.module('expenseApp.Controllers')
 
       $scope.expenseCategories = [];
 
+      function getExpenseCategories() {
+          ExpenseCategory.getAllExpenseCategories().then(
+              function (success) {
+                  console.log('success finding all expense categories');
+                  console.log(success);
+                  success.data.forEach(function (category) {
+                      $scope.expenseCategories.push(category.ExpenseCategoryName);
+                  });
+                  $scope.setPolicyView();
+              },
+              function (error) {
+                  console.log('failed to find expense categories');
+                  console.log(error);
+                  LogError.logError({ username: Authentication.getUser(), endpoint: error.config.url, error: error.statusText }).then(
+                    function (success) { },
+                    function (error) { });
+              });
+      }
+      getExpenseCategories();
+
       /**
       * show policy depending on the expense type chosen
       */
-      $scope.policyIs = {
-          mileage: selectedType === $scope.expenseCategories[0],
-          perdiem: selectedType === $scope.expenseCategories[1],
-          transportation: selectedType === $scope.expenseCategories[2],
-          lodging: selectedType === $scope.expenseCategories[3],
-          parking: selectedType === $scope.expenseCategories[4],
-          entertainment: selectedType === $scope.expenseCategories[5],
-          meals: selectedType === $scope.expenseCategories[6],
-          airfare: selectedType === $scope.expenseCategories[7],
-          other: selectedType === $scope.expenseCategories[8]
-      };
+      $scope.setPolicyView = function () {
+          $scope.policyIs = {
+              mileage: $scope.policyType === $scope.expenseCategories[0],
+              perdiem: $scope.policyType === $scope.expenseCategories[1],
+              transportation: $scope.policyType === $scope.expenseCategories[2],
+              lodging: $scope.policyType === $scope.expenseCategories[3],
+              parking: $scope.policyType === $scope.expenseCategories[4],
+              entertainment: $scope.policyType === $scope.expenseCategories[5],
+              meals: $scope.policyType === $scope.expenseCategories[6],
+              airfare: $scope.policyType === $scope.expenseCategories[7],
+              other: $scope.policyType === $scope.expenseCategories[8]
+          };
+      }
 
       /**
        * Close out of the Policy modal.
@@ -35,20 +57,5 @@ angular.module('expenseApp.Controllers')
       $scope.ok = function () {
           $modalInstance.dismiss('Form was cancelled.');
       };
-
-      function getExpenseCategories() {
-          ExpenseCategory.getAllExpenseCategories().then(
-              function (success) {
-                  success.data.forEach(function (category) {
-                      $scope.expenseCategories.push(category.ExpenseCategoryName);
-                  });
-              },
-              function (error) {
-                  LogError.logError({ username: Authentication.getUser(), endpoint: error.config.url, error: error.statusText }).then(
-                    function (success) { },
-                    function (error) { });
-              });
-      }
-      getExpenseCategories();
 
   }]);
