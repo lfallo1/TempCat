e20aa6557f940e5f850c3318c33339b1a6163b85
jnkroeker <jnkroeker@gmail.com>
2015-02-20 11:05:41 -0500

    backend unit tests for ErrorController.cs

---
+++ catexpense/UnitTestProject/BackEnd_UnitTests/ControllerTests/ErrorControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject.BackEnd_UnitTests.ControllerTests
+{
+    [TestClass]
+    public class ErrorControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
