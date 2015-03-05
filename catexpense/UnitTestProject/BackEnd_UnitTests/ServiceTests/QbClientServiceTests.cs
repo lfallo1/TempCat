0f6c56da7e503af614e6e52f7e7b812ec80a44d5
unknown <lFallon@PC30093.catalystsolves.com>
2015-03-05 10:02:31 -0500

    add tests for QbServices

---
+++ catexpense/UnitTestProject/BackEnd_UnitTests/ServiceTests/QbClientServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CatExpenseFront.Models;
+using CatExpenseFront.Repository;
+using CatExpenseFront.Services;
+using CatExpenseFront.Services.Interfaces.Base;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NUnit.Framework;
+using UnitTestProject.BackEnd_UnitTests.ServiceTests.Base;
+
+namespace UnitTestProject.BackEnd_UnitTests.ServiceTests
+{
+    [TestFixture]
+    public class QbClientServiceTests : IServiceTestBase<QbClient>
+    {
+        public override IService<QbClient> GetIServiceInstanceWithRepoParamater()
+        {
+            return new QbClientService(mockRepository.Object);
+        }
+
+        public override IService<QbClient> GetInstanceWithEmptyConstructor()
+        {
+            return new QbClientService();
+        }
+
+        public override Mock<IRepository<QbClient>> GetMockRepository()
+        {
+            return new Mock<IRepository<QbClient>>();
+        }
+
+        public override QbClient GetObjectInstance()
+        {
+            return new QbClient();
+        }
+
+        public override IQueryable<QbClient> GetObjectList()
+        {
+            return new List<QbClient>().AsQueryable();
+        }
+    }
+}
---
+++ catexpense/UnitTestProject/BackEnd_UnitTests/ServiceTests/QbVendorServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CatExpenseFront.Models;
+using CatExpenseFront.Repository;
+using CatExpenseFront.Services;
+using CatExpenseFront.Services.Interfaces.Base;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NUnit.Framework;
+using UnitTestProject.BackEnd_UnitTests.ServiceTests.Base;
+
+namespace UnitTestProject.BackEnd_UnitTests.ServiceTests
+{
+    [TestFixture]
+    public class QbVendorServiceTests : IServiceTestBase<QbVendor>
+    {
+        public override IService<QbVendor> GetIServiceInstanceWithRepoParamater()
+        {
+            return new QbVendorService(mockRepository.Object);
+        }
+
+        public override IService<QbVendor> GetInstanceWithEmptyConstructor()
+        {
+            return new QbVendorService();
+        }
+
+        public override Mock<IRepository<QbVendor>> GetMockRepository()
+        {
+            return new Mock<IRepository<QbVendor>>();
+        }
+
+        public override QbVendor GetObjectInstance()
+        {
+            return new QbVendor();
+        }
+
+        public override IQueryable<QbVendor> GetObjectList()
+        {
+            return new List<QbVendor>().AsQueryable();
+        }
+    }
+}
