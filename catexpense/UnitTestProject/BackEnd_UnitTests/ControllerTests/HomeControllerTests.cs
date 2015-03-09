6c973646105fb0d79e3af456804257e9e726ffa6
shaneklovell <slovell@catalystitservices.com>
2015-03-09 15:47:14 -0400

    Added unit test for home controller

--- catexpense/UnitTestProject/BackEnd_UnitTests/ControllerTests/HomeControllerTests.cs
+++ catexpense/UnitTestProject/BackEnd_UnitTests/ControllerTests/HomeControllerTests.cs
@@ -5,19 +5,19 @@ using NUnit.Framework;
 namespace UnitTestProject.BackEnd_UnitTests.ControllerTests
 {
     [TestFixture]
-    public class HotTowelControllerTests
+    public class HomeControllerTests
     {
         private HomeController controller = new HomeController();
 
-        //[Test]
+        [Test]
         public void IndexActionResultTest()
         {
             // Act
-            //var response = controller.Index();
+            var response = controller.Index();
 
             // Assert
-            //Assert.IsNotNull(response);
-            //Assert.AreEqual(typeof(ViewResult), response.GetType());
+            Assert.IsNotNull(response);
+            Assert.AreEqual(typeof(ViewResult), response.GetType());
         }
     }
 }
