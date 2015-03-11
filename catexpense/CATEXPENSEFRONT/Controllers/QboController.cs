c425b4203df697c46b12f76f4dcd214380b46dca
shaneklovell <slovell@catalystitservices.com>
2015-03-11 10:52:24 -0400

    adding unit tests for QboController

--- catexpense/CATEXPENSEFRONT/Controllers/QboController.cs
+++ catexpense/CATEXPENSEFRONT/Controllers/QboController.cs
@@ -17,6 +17,7 @@ namespace CatExpenseFront.Controllers
     {
         private IQbVendorService vendorService;
         private IQbClientService clientService;
+        private QuickBooksRequest qRequest;
 
         /// <summary>
         /// Default Constructor
@@ -25,6 +26,7 @@ namespace CatExpenseFront.Controllers
         {
             vendorService = new QbVendorService();
             clientService = new QbClientService();
+            qRequest = new QuickBooksRequest();
         }
 
 
@@ -32,11 +34,26 @@ namespace CatExpenseFront.Controllers
         /// Constructor that accepts Category service
         /// </summary>
         /// <param name="iService"></param>
-        /*public QboController(IQbVendorService iVendorService, IQbClientService iClientService)
+        public QboController(IQbVendorService iVendorService, IQbClientService iClientService)
         {
             vendorService = iVendorService;
             clientService = iClientService;
-        }*/
+            qRequest = new QuickBooksRequest();
+        }
+
+        /// <summary>
+        /// Constructor that accepts all consumed services and classes for testing purposes
+        /// </summary>
+        /// <param name="iVendorService"></param>
+        /// <param name="iClientService"></param>
+        /// <param name="_qRequest"></param>
+        public QboController(IQbVendorService iVendorService, IQbClientService iClientService, 
+            QuickBooksRequest _qRequest)
+        {
+            vendorService = iVendorService;
+            clientService = iClientService;
+            qRequest = _qRequest;
+        }
         [HttpGet]
         [ActionName("qbo")]
         [Route("api/qbo/")]
---
+++ catexpense/UnitTestProject/BackEnd_UnitTests/ControllerTests/QBOControllerTests.cs
@@ -0,0 +1,72 @@
+﻿using CatExpenseFront.Controllers;
+using CatExpenseFront.Models;
+using CatExpenseFront.Services.Interfaces;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject.BackEnd_UnitTests.ControllerTests
+{
+    public class QBOControllerTests
+    {
+        private Mock<QuickBooksRequest> qb = new Mock<QuickBooksRequest>();
+        private QboController controller;
+        private Mock<IQbVendorService> vendorService = new Mock<IQbVendorService>();
+        private Mock<IQbClientService> clientService = new Mock<IQbClientService>();
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            controller = new QboController(vendorService.Object, clientService.Object, qb.Object);
+            
+        }
+
+        [TestFixtureTearDown]
+        public void CleanUp()
+        {
+            controller.Dispose();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            controller.ModelState.Clear();
+        }
+
+        [Test]
+        public void EmptyQboConstructorTest()
+        {
+            var emptyController = new QboController();
+            Assert.IsNotNull(emptyController);
+            Assert.AreEqual(typeof(QboController), emptyController.GetType());
+        }
+
+        [Test]
+        public void TwoQboConstructorTest()
+        {
+            var twoConstructor = new QboController(vendorService.Object, clientService.Object);
+            Assert.IsNotNull(twoConstructor);
+            Assert.AreEqual(typeof(QboController), twoConstructor.GetType());
+        }
+
+        [Test]
+        public void QboConstructorTest()
+        {
+            Assert.AreEqual(typeof(QboController), controller.GetType());
+            Assert.IsNotNull(controller);
+        }
+
+        [Test]
+        [ExpectedException(typeof(System.MissingMethodException))]
+        public void GetQboTest()
+        {
+           
+            controller.GetQbo();
+            qb.Verify(x => controller.GetQbo());
+        }
+    }
+}
